70b05ed38647b0af65e0a1d6f0bac50fb53b4e3f
Supichaya <gen.supichaya@gmail.com>
2019-04-10 22:28:52 +0700

    update viewing and registration

--- registration.sql
+++ registration.sql
@@ -1,21 +1,11 @@
 CREATE TABLE IF NOT EXISTS `std6102041510097`.`registration` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `client_id` INT(11) NULL DEFAULT NULL,
-  `branch_id` INT(11) NULL DEFAULT NULL,
-  `staff_id` INT(11) NULL DEFAULT NULL,
+  `property_for_rent_id` INT(11) NULL DEFAULT NULL,
   `dateJoined` DATE NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `client_id` (`client_id` ASC),
-  INDEX `branch_id` (`branch_id` ASC),
-  INDEX `staff_id` (`staff_id` ASC),
-  CONSTRAINT `registration_ibfk_1`
-    FOREIGN KEY (`client_id`)
-    REFERENCES `std6102041510097`.`client` (`id`),
-  CONSTRAINT `registration_ibfk_2`
-    FOREIGN KEY (`branch_id`)
-    REFERENCES `std6102041510097`.`branch` (`id`),
-  CONSTRAINT `registration_ibfk_3`
-    FOREIGN KEY (`staff_id`)
-    REFERENCES `std6102041510097`.`staff` (`id`))
+  INDEX `client_id` (`property_for_rent_id` ASC),
+  CONSTRAINT `fk_registration_pfr_id`
+    FOREIGN KEY (`property_for_rent_id`)
+    REFERENCES `std6102041510097`.`property_for_rent` (`id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
--- viewing.sql
+++ viewing.sql
@@ -1,13 +1,19 @@
 CREATE TABLE IF NOT EXISTS `std6102041510097`.`viewing` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `client_id` INT(11) NULL DEFAULT NULL,
-  `property_id` INT(11) NULL DEFAULT NULL,
+  `property_for_rent_id` INT(11) NULL DEFAULT NULL,
   `view_date` DATE NULL DEFAULT NULL,
   `comment` TEXT NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   INDEX `client_id` (`client_id` ASC),
-  CONSTRAINT `viewing_ibfk_1`
+  INDEX `fk_viewing_pfr_id_idx` (`property_for_rent_id` ASC),
+  CONSTRAINT `fk_viewing_client_id`
     FOREIGN KEY (`client_id`)
-    REFERENCES `std6102041510097`.`client` (`id`))
+    REFERENCES `std6102041510097`.`client` (`id`),
+  CONSTRAINT `fk_viewing_pfr_id`
+    FOREIGN KEY (`property_for_rent_id`)
+    REFERENCES `std6102041510097`.`property_for_rent` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
